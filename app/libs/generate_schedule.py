b6ec7c40ae2bf0da2c7d1e5d9db9d1d1d62de921
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2024-01-10 20:59:12 +0000

    fixing files

--- app/libs/generate_schedule.py
+++ app/libs/generate_schedule.py
@@ -1,16 +1,43 @@
 from generate_staff import convert_time, STAFF
 from prettytable import PrettyTable
 
-schedule_print = PrettyTable(["", "9-11", "11-1", "1-2", "2-4", "4-6", "6-8"])
+staff = STAFF.staff_list
+compare_time = [[900, 1100], [1100, 1300], [1300, 1400], [1400, 1600], [1600, 1800], [1800, 2000]]
 
+def isavailable(employee, day, time):
+    staff_in, staff_out, staff_break, compare_in, compare_out = employee[day + "-hours"][0], employee[day + "-hours"][1], employee[day + "-break"][0], time[0], time[1]
+    if not isinstance(staff_break, int): staff_break = 0
+    if isinstance(staff_in, int) and isinstance(staff_out, int):
+        if staff_in >= compare_in and staff_out >= compare_out:
+            if (staff_break < compare_in or staff_break > compare_out) or staff_break == 0:
+                return True
+
+def staff_to_schedule(schedule, day):
+    # REWORK THIS
+    occupied_employees = []
+    available_employees = []
+    
+    for shift in compare_time:
+        temp_list = []
+        for employee in staff:
+            if isavailable(employee, day, shift) and not employee["position"] in ["security", "shelver"]:
+                temp_list.append(employee)
+        available_employees.append(temp_list)
+    
+    for employee in available_employees[0]:
+        print(employee["name"])
+
+schedule_print = PrettyTable(["", "9-11", "11-1", "1-2", "2-4", "4-6", "6-8"])
 monday_template = [
     ["pick-up window", "", "", "", "", "", "",],
     ["floor lead", "", "", "", "", "", "",],
     ["service pt 1", "", "", "", "", "", "",],
     ["service pt 1", "", "", "", "", "", "",],
     ["service pt 2", "", "", "", "", "", "",],
-    ["service pt 2", "", "", "", "", "", "",],
+    ["service pt 2", "", "", "", "", "", "",]
 ]
+
+staff_to_schedule(monday_template, "monday")
 
 for d in monday_template:
     schedule_print.add_row(d)
--- app/libs/generate_staff.py
+++ app/libs/generate_staff.py
@@ -10,8 +10,7 @@ class Staff:
             self.staff_list = [row for row in reader]
             for employee in self.staff_list:
                 for data in employee:
-                    if "hours" in data or "break" in data:
-                        employee[data] = self.format_time(employee[data])
+                    if "hours" in data or "break" in data: employee[data] = self.format_time(employee[data])
     
     def format_time(self, input_time):
         output_time = input_time
@@ -19,10 +18,8 @@ class Staff:
             output_time = output_time.split(")/(")
             for h1 in range(len(output_time)):
                 output_time[h1] = output_time[h1].replace("(", "").replace(")", "")
-                if "/" in output_time[h1]:
-                    output_time[h1] = output_time[h1].split("/")
-        if "/" in output_time:
-            output_time = output_time.split("/")
+                if "/" in output_time[h1]: output_time[h1] = output_time[h1].split("/")
+        if "/" in output_time: output_time = output_time.split("/")
         if isinstance(output_time, list):
             for h1 in range(len(output_time)):
                 if isinstance(output_time[h1], list):
@@ -32,18 +29,16 @@ class Staff:
                                 output_time[h1][h2] = output_time[h1][h2].split("-")
                                 if isinstance(output_time[h1][h2], list):
                                     for h3 in range(len(output_time[h1][h2])):
-                                        if "am" in output_time[h1][h2][h3] or "pm" in output_time[h1][h2][h3]:
-                                            output_time[h1][h2][h3] = convert_time(output_time[h1][h2][h3], to_24=True)
+                                        if "am" in output_time[h1][h2][h3] or "pm" in output_time[h1][h2][h3]: output_time[h1][h2][h3] = convert_time(output_time[h1][h2][h3], to_24=True)
                                 else:
-                                    if "am" in output_time[h1][h2] or "pm" in output_time[h1][h2]:
-                                        output_time[h1][h2] = convert_time(output_time[h1][h2], to_24=True)
+                                    if "am" in output_time[h1][h2] or "pm" in output_time[h1][h2]: output_time[h1][h2] = convert_time(output_time[h1][h2], to_24=True)
                 else:
                     if "am" in output_time[h1].lower() or "pm" in output_time[h1].lower():
                         if not "Off" in output_time[h1] or not "None" in output_time[h1]:
                             output_time[h1] = output_time[h1].split("-")
                             for h2 in range(len(output_time[h1])):
                                 if "am" in output_time[h1][h2] or "pm" in output_time[h1][h2]:
-                                        output_time[h1][h2] = convert_time(output_time[h1][h2], to_24=True)
+                                    output_time[h1][h2] = convert_time(output_time[h1][h2], to_24=True)
         else:
             if "am" in output_time.lower() or "pm" in output_time.lower():
                 output_time = output_time.split("-")
@@ -58,80 +53,9 @@ class Staff:
 
 def convert_time(input_time, to_24):
     output_time = input_time
-    if not ":" in output_time:
-        output_time = output_time[:-2] + ":00" + output_time[-2:]
-    if to_24:
-        output_time = datetime.strptime(output_time.upper(), "%I:%M%p").strftime("%H%M")
-    else:
-        output_time = datetime.strptime(output_time, "%H%M").strftime("%I:%M%p")
-    return output_time
-
-# Working out formatting
-employee = {
-    "name": "Chris Wright",
-    "position": "Full-Time",
-    "sunday-hours": "Off/Off/2pm-6pm",
-    "monday-hours": "2pm-8pm",
-    "tuesday-hours": "9am-5:30pm",
-    "wednesday-hours": "9am-5:30pm",
-    "thursday-hours": "9am-5:30pm",
-    "friday-hours": "(9am-6pm/9am-6pm)/(Off/Off)/(2pm-6pm/2pm-6pm)",
-    "saturday-hours": "(Off/Off)/(9am-6pm/9am-6pm)/(Off/Off)",
-    "sunday-break": "None/None/None",
-    "monday-break": "5pm",
-    "tuesday-break": "1pm",
-    "wednesday-break": "12pm",
-    "thursday-break": "12pm",
-    "friday-break": "(12pm/12pm)/(None/None)/(None/None)",
-    "saturday-break": "(None/None)/(12pm/12pm)/(None/None)"
-}
-
-def format_time(input_time):
-    output_time = input_time
-    if "(" in output_time:
-        output_time = output_time.split(")/(")
-        for h1 in range(len(output_time)):
-            output_time[h1] = output_time[h1].replace("(", "").replace(")", "")
-            if "/" in output_time[h1]:
-                output_time[h1] = output_time[h1].split("/")
-    if "/" in output_time:
-        output_time = output_time.split("/")
-    if isinstance(output_time, list):
-        for h1 in range(len(output_time)):
-            if isinstance(output_time[h1], list):
-                for h2 in range(len(output_time[h1])):
-                    if "am" in output_time[h1][h2].lower() or "pm" in output_time[h1][h2].lower():
-                        if not "Off" in output_time[h1][h2] or not "None" in output_time[h1][h2]:
-                            output_time[h1][h2] = output_time[h1][h2].split("-")
-                            if isinstance(output_time[h1][h2], list):
-                                for h3 in range(len(output_time[h1][h2])):
-                                    if "am" in output_time[h1][h2][h3] or "pm" in output_time[h1][h2][h3]:
-                                        output_time[h1][h2][h3] = convert_time(output_time[h1][h2][h3], to_24=True)
-                            else:
-                                if "am" in output_time[h1][h2] or "pm" in output_time[h1][h2]:
-                                    output_time[h1][h2] = convert_time(output_time[h1][h2], to_24=True)
-            else:
-                if "am" in output_time[h1].lower() or "pm" in output_time[h1].lower():
-                    if not "Off" in output_time[h1] or not "None" in output_time[h1]:
-                        output_time[h1] = output_time[h1].split("-")
-                        for h2 in range(len(output_time[h1])):
-                            if "am" in output_time[h1][h2] or "pm" in output_time[h1][h2]:
-                                    output_time[h1][h2] = convert_time(output_time[h1][h2], to_24=True)
-    else:
-        if "am" in output_time.lower() or "pm" in output_time.lower():
-            output_time = output_time.split("-")
-            if isinstance(output_time, list):
-                for h1 in range(len(output_time)):
-                    if "am" in output_time[h1].lower() or "pm" in output_time[h1].lower():
-                        output_time[h1] = convert_time(output_time[h1], to_24=True)
-            else:
-                if "am" in output_time.lower() or "pm" in output_time.lower():
-                    output_time = convert_time(output_time, to_24=True)
-    return output_time
-
-for data in employee:
-    if "hours" in data or "break" in data:
-        employee[data] = format_time(employee[data])
-    # print(employee[data])
+    if not ":" in output_time: output_time = output_time[:-2] + ":00" + output_time[-2:]
+    if to_24: output_time = datetime.strptime(output_time.upper(), "%I:%M%p").strftime("%H%M")
+    else: output_time = datetime.strptime(output_time, "%H%M").strftime("%I:%M%p")
+    return int(output_time)
 
 STAFF = Staff(join(dirname(realpath(__file__)), "staff_info.csv"))
