9031b4827eaf996fc1147b427dabb46c1d968669
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2024-02-02 17:57:13 -0500

    Update generator.py

--- app/libs/generator.py
+++ app/libs/generator.py
@@ -1,4 +1,4 @@
-from csv_generators import convert_time, STAFF, TEMPLATE, all_day_locations
+from csv_generators import convert_time, STAFF, TEMPLATE, all_day_locations, all_compare_times
 from os.path import dirname, join, realpath
 from prettytable import PrettyTable
 from random import randint
@@ -16,14 +16,25 @@ def create_schedule(date):
     day_names = [
         "sunday1", "sunday2", "sunday3", "monday", "tuesday", "wednesday", "thursday", "friday1a", "friday1b", "friday2a", "friday2b", "friday3a", "friday3b", "saturday1a", "saturday1b", "saturday2a", "saturday2b", "saturday3a", "saturday3b"
     ]
+    
+    dict_avoids = [
+        "name","position","sunday-hours","monday-hours","tuesday-hours","wednesday-hours","thursday-hours","friday-hours","saturday-hours","sunday-break","monday-break","tuesday-break","wednesday-break","thursday-break","friday-break","saturday-break", "pickup-window-time", "floor-lead-time", "sp1a-time", "sp1b-time", "sp1-time", "sp2a-time", "sp2b-time", "sp2-time", "initials", "rank"
+    ]
 
+    loc_range = 6
     date_day = date[0].lower()
-    if day_names.index(date_day) <= 2:
+    if "sunday" in date_day:
         schedule_header = schedule_header_list[0]
-    if day_names.index(date_day) >= 3 and day_names.index(date_day) <= 6:
-        schedule_header = schedule_header_list[1]
-    if day_names.index(date_day) >= 7:
+        compare_time = all_compare_times[0]
+        hour_range = [1, 5]
+    elif "friday" in date_day or "saturday" in date_day:
         schedule_header = schedule_header_list[2]
+        compare_time = all_compare_times[1]
+        hour_range = [1, 7]
+    else:
+        schedule_header = schedule_header_list[1]
+        compare_time = all_compare_times[2]
+        hour_range = [1, 7]
     
     schedule_print = PrettyTable(schedule_header)
     
@@ -44,21 +55,28 @@ def create_schedule(date):
         for i in weekday_template:
             i.pop()
             i.pop()
-    
-    for loc in range(6):
-        for employee in staff:
-            pass
-        print(date_day + location_names[loc])
-    
-    for d in range(6):
-        if d in [0, 1, 3, 5]:
+    print(len(compare_time))
+    for loc in range(loc_range):
+        day_loc = date_day + location_names[loc]
+        for hour in range(hour_range[0], hour_range[1]):
+            for employee in staff:
+                if len(employee[day_loc]) != 0:
+                    for k, v in employee.items():
+                        if k == day_loc and employee[k][0] == compare_time[hour - 1]:
+                            weekday_template[loc][hour] += employee[k][1]
+            if weekday_template[loc][hour][-3:-1] == "pm":
+                weekday_template[loc][hour] = weekday_template[loc][hour][:-1]
+
+    for d in range(len(weekday_template)):
+        if d in [0, 1, 3, 5, 6]:
             schedule_print.add_row(weekday_template[d], divider=True)
         else:
             schedule_print.add_row(weekday_template[d])
     
     return schedule_print
 
-date = ["Monday", "January 29, 2024"]
+# some weekends/fridays don't work
+date = ["Sunday3", "January 29, 2024"]
 
 schedule = create_schedule(date)
 
